b3920e5f9be65358a8e200d437107b4f62a6c1df
glove.tux <alexzsk@outlook.com>
2024-09-07 15:22:09 -0400

    Update reap.zig

--- ascii/reap.zig
+++ ascii/reap.zig
@@ -1,11 +1,15 @@
+const std = @import("std");
 
-                                                                     ______
-                                                                     |    |    
-           .--------------------------------------------------------.|    |    
-           |                                                         |    |    
-           |   "whatsoever a man soweth, that shall he also reap."   |    |   
-           |                                                       ._|____|_.  
-           '-------------------------------------------------------\\|o_o |    
-                                                                     |:_/ |    
-                                                                    //   \ \  
-                                                                   (|     | )
+pub fn main() void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("                                                                     ______\n", .{});
+    try stdout.print("                                                                     |    |    \n", .{});
+    try stdout.print("           .--------------------------------------------------------.|    |    \n", .{});
+    try stdout.print("           |                                                         |    |    \n", .{});
+    try stdout.print("           |   \"whatsoever a man soweth, that shall he also reap.\"   |    |   \n", .{});
+    try stdout.print("           |                                                       ._|____|_.  \n", .{});
+    try stdout.print("           '-------------------------------------------------------\\|o_o |    \n", .{});
+    try stdout.print("                                                                     |:_/ |    \n", .{});
+    try stdout.print("                                                                    //   \\ \\  \n", .{});
+    try stdout.print("                                                                   (|     | )\n", .{});
+}
